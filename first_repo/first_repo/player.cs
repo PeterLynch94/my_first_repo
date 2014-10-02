9e8954f5e5d3fda6617246698499d9986204c5d9
Joe Nicholas <jnicholas182@gmail.com>
2014-10-02 12:33:22 +0100

    Added player class!

---
+++ first_repo/first_repo/player.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace first_repo
+{
+	public class player
+	{
+		int health;
+		int spriteCount;
+		int speed;
+		float x, y;
+		
+		
+		
+		public player (float x, float y, int h, int sp)
+		{
+			
+		}
+		
+		public void setX(float x)
+		{
+			this.x = x;
+		}
+		
+		public void setY(float y)
+		{
+			this.y = y;
+		}
+		
+		public float getX()
+		{
+			return this.x;
+		}
+		
+		public float getY()
+		{
+			return this.x;
+		}
+	}
+}
+
